6f98b8f7f23b01bbdbee36c66af7b2e9cd4e66d0
OwenABC1 <owennsullivan@icloud.com>
2024-09-04 14:00:36 -0700

    Update Questions

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
 	
+
+	$(".Correct").click(function() {
+		$(".Correct").addClass("btn-success");
+		$(".Correct").removeClass("btn-primary");
+	});
+	
+	$(".Incorrect").click(function() {
+		$(".Correct").addClass("btn-success");
+		$(".Correct").removeClass("btn-primary");
+		$(".Incorrect").addClass("btn-danger");
+		$(".Incorrect").removeClass("btn-primary");
+		$(".alert").removeClass("helper");
+		$(".alert").addClass("Helping");
+	});
+	
 });
